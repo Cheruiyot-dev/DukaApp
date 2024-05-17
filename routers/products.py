8d8562faf73c99cb2bdc80608295c7f898a627d2
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-05-17 16:53:20 +0300

    create customer route

--- routers/products.py
+++ routers/products.py
@@ -51,23 +51,6 @@ async def get_product_by_id(product_id: int, db: Session = Depends(get_db), curr
     return product
 
 
-# @router.put("/products/{product_id}")
-# async def update_product(product_id: int, product_update: ProductUpdate,
-#                           db: Session = Depends(get_db)) -> Dict[str, ProductUpdate]:
-#     product_to_update = db.query(Product).filter(Product.id == product_id).first()
-#     print(product_to_update)
-#     if product_to_update is None:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail="Product does not exist")
-#     # Update the product attributes
-#     for field, value in product_update.model_dump().items():
-#         setattr(product_to_update, field, value)
-
-#     db.commit()
-#     db.refresh(product_to_update)
-#     return product_to_update
-
 @router.delete("/products/{product_id}")
 async def delete_product(product_id: int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
     # Retrieve the product from the database
