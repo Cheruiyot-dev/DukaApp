2d3eac2157e4a7b041d59cc9c7c98fc9cab34c90
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-05-20 22:33:01 +0300

    new changes

--- routers/auth.py
+++ routers/auth.py
@@ -3,7 +3,7 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from schemas import Token
 from database import get_db
-from models import Customer
+from models import SalesAgent
 from oauth2 import create_access_token
 
 router = APIRouter(tags=['Authentication'])
@@ -12,14 +12,14 @@ router = APIRouter(tags=['Authentication'])
 @router.post('/login', response_model=Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
           db: Session = Depends(get_db)):
-    customer = db.query(Customer).filter(Customer.username == user_credentials.username).first()
+    sale_agent = db.query(SalesAgent).filter(SalesAgent.username == user_credentials.username).first()
 
-    if not customer:
+    if not sale_agent:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid Credentials"
         )
 
-    access_token = create_access_token(data={"username": customer.username})
+    access_token = create_access_token(data={"username": sale_agent.username})
 
     return {"access_token": access_token, "token_type": "bearer"}
